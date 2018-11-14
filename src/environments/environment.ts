2f4f3e7269f697f1deba010ef71a4ad980cd8513
madi-madi <ssss_-_2010@hotmail.com>
2018-11-14 10:34:27 +0200

    ng environment firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
-     apiKey: "AIzaSyChVHnkFv7VNO7HqAdmsH8OEhZhyyt2PzA",
-    authDomain: "vue-chat-1dae1.firebaseapp.com",
-    databaseURL: "https://vue-chat-1dae1.firebaseio.com",
-    projectId: "vue-chat-1dae1",
-    storageBucket: "vue-chat-1dae1.appspot.com",//vue-chat-1dae1.appspot.com
-    messagingSenderId: "174322079757"
+     apiKey: "firebase apiKey",
+    authDomain: "example.firebaseapp.com",
+    databaseURL: "example.firebaseio.com",
+    projectId: "ng-chat",
+    storageBucket: "example.appspot.com",//vue-chat-1dae1.appspot.com
+    messagingSenderId: "0000000000"
  }
 };
 
